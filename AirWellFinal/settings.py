9aa4cf8980fefd3361e2a35d2005572f6a2b98aa
Jikku James <jikkujames34@gmail.com>
2024-04-02 10:22:41 +0530

    cors headers add

--- AirWellFinal/settings.py
+++ AirWellFinal/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AirWellApp'
+    'AirWellApp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'AirWellFinal.urls'
@@ -138,3 +140,7 @@ MEDIA_ROOT = str(BASE_DIR) + '/media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'https://airwellmosquito.com',
+]
