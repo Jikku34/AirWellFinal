56a564a1d08eaf71ab41565bcd5aa7503761e7c0
Jikku James <jikkujames34@gmail.com>
2024-04-02 16:25:26 +0530

    debug test8

--- AirWellApp/views.py
+++ AirWellApp/views.py
@@ -357,8 +357,7 @@ def product_view(request, id):
     """
     categories_with_products = ProductCategoryModel.objects.prefetch_related('productmodel_set').all()
     product = ProductModel.objects.prefetch_related('productimagemodel_set').get(product_id=id)
-    return render(request, 'user/product_view.html',
-                  {'product': product, 'categories_with_products': categories_with_products})
+    return render(request, 'user/product_view.html',{'product': product, 'categories_with_products': categories_with_products})
 
 
 def user_about_page(request):
